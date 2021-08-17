25caf64fb0de79f21c4fada9c2e3af750d79f620
Brett Davis <brett.p.davis101@gmail.com>
2021-08-17 18:52:59 -0500

    Played around a bit with a particle to make the full cauldron look better

--- Assets/Scripts/CauldronLiguid.cs
+++ Assets/Scripts/CauldronLiguid.cs
@@ -6,6 +6,7 @@ public class CauldronLiguid : MonoBehaviour
 {
     public List<string> names;
     public ParticleSystemRenderer particle;
+    public GameObject bubbles;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,9 +21,15 @@ public class CauldronLiguid : MonoBehaviour
 
     void OnParticleCollision(GameObject other)
     {
+        // TODO: Change this to /copy/ the material, not match it
+        // This will preserve original base material when cauldron
+        // starts to change colors
         this.GetComponent<MeshRenderer>().material = particle.material;
+        bubbles.GetComponent<MeshRenderer>().material = particle.material;
         if (transform.position.y < -.9)
             transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+        if (transform.position.y == -.9)
+            bubbles.SetActive(true);
     }
 
     void OnCollisionEnter(Collision collide) {
