c29eca9613a638ba5651ec268b0d76bbd72e3cdf
Brett Davis <brett.p.davis101@gmail.com>
2021-08-23 15:11:43 -0500

    Sets up data structure for storing and passing on ingredient info

--- Assets/Scripts/CauldronLiquid.cs
+++ Assets/Scripts/CauldronLiquid.cs
@@ -7,37 +7,42 @@ public class CauldronLiquid : MonoBehaviour
     public List<string> names;
     public ParticleSystemRenderer particle;
     public GameObject bubbles;
+    private Material liquidMat;
+    private Material bubbleMat;
     // Start is called before the first frame update
     void Start()
     {
         names = new List<string>();
+        liquidMat = this.GetComponent<MeshRenderer>().material;
+        bubbleMat = bubbles.gameObject.GetComponent<ParticleSystemRenderer>().material;
     }
 
     // Update is called once per frame
     void Update()
     {
-    }
-
-    void OnParticleCollision(GameObject other)
-    {
-        this.GetComponent<MeshRenderer>().material.CopyPropertiesFromMaterial(particle.material);
-        bubbles.gameObject.GetComponent<ParticleSystemRenderer>().material.CopyPropertiesFromMaterial(particle.material);
-        if (transform.position.y < -.9)
-            transform.Translate(Vector3.up * Time.deltaTime, Space.World);
         if (transform.position.y >= -.9) {
-            transform.position = new Vector3(transform.position.x, (float)-.9, transform.position.z);
+            transform.position = new Vector3(transform.position.x, (float)-.89, transform.position.z);
             bubbles.SetActive(true);
         }
     }
 
+    void OnParticleCollision(GameObject other)
+    {
+         if (transform.position.y < -.9) {
+            transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+            liquidMat.CopyPropertiesFromMaterial(particle.material);
+            bubbleMat.CopyPropertiesFromMaterial(particle.material);
+        }
+    }
+
     void OnCollisionEnter(Collision collide) {
-        if (transform.position.y >= -.9) {
+        if (transform.position.y >= -.99) {
             if (collide.gameObject.tag == "Ingredient") {
                 names.Add(collide.gameObject.name);
+                Debug.Log(collide.gameObject.GetComponent<Ingredient>().Name);
+                liquidMat.SetColor("_Color", collide.gameObject.GetComponent<Ingredient>().propertyColor);
+                bubbleMat.SetColor("_Color", liquidMat.GetColor("_Color"));
                 Destroy(collide.gameObject);
-                foreach (var n in names) {
-                    Debug.Log(n);
-                }
             }
         }
     }
